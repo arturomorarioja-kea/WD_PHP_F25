cfc55a046506ba8ad8f62a8d823ee1869f09b0f5
Arturo Mora-Rioja - KEA <amri.keadigital@gmail.com>
2025-05-30 18:14:13 +0200

    Add files via upload

---
+++ ExamMaterials/array.php
@@ -0,0 +1,13 @@
+<?php
+
+$countries = [
+    ['name' => 'Denmark', 'cities' => [
+        'Copenhagen', 'Aarhus'
+    ]],
+    ['name' => 'Sweden', 'cities' => [
+        'Stockholm', 'Gothemburg'
+    ]],
+    ['name' => 'Norway', 'cities' => [
+        'Oslo'
+    ]]
+];
---
+++ ExamMaterials/form.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Exam</title>
+</head>
+<body>
+    <header>
+        <h1>Form</h1>
+    </header>
+    <main>
+        <form action="form.php" method="">
+            <div>
+                <label for="txtName">Name</label>
+                <input type="text" id="txtName">
+            </div>
+            <div>
+                <label for="txtEmail">Email</label>
+                <input type="email" id="txtEmail">
+            </div>
+            <div>
+                <button type="submit">Submit form</button>
+            </div>
+        </form>
+    </main>
+</body>
+</html>
